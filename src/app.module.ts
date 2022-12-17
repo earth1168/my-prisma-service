95e3640b7bbf348acd8e92c4cf84ca75549510fa
Pimanus Kutragoon <pimanus1234@gmail.com>
2022-12-17 18:30:58 +0700

    Add Dog and Owner module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ApolloServerPluginLandingPageLocalDefault} from 'apollo-server-core';
+import { PrismaModule } from './prisma/prisma.module';
+import { DogModule } from './dog/dog.module';
+import { OwnerModule } from './owner/owner.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/types/graphql.ts'),
+        outputAs: 'class',
+      },
+      }),
+      PrismaModule,
+      DogModule,
+      OwnerModule,
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
